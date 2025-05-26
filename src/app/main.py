42cddee3780e70ee2920616e908a3b118aa764ab
Juan Manuel Carballo <jm.carb@gmail.com>
2025-05-26 11:02:39 -0600

    Agregado preprocesamiento a factura

--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,10 @@
-import src.app.scan as scan
+import src.app.app as app
 import tkinter as tk
 
 
 def main():
     root = tk.Tk()
-    app = scan.ScanApp(root)
+    app = app.ScanApp(root)
     app.compose()
     app.show_frame()
     root.mainloop()
